71c67dcd0fc50c9d06599fd3efee1857f688e383
montassier-paul <paul.montassier.pro@gmail.com>
2022-11-09 14:53:07 -0500

    lint errors fixed

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -148,7 +148,7 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
-  
+
     def test_update_account(self):
         """It should Update an existing Account"""
         # create an Account to update
@@ -189,4 +189,3 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Check for the CORS header
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
- 
